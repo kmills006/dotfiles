e5729a6be8c38d27775a18d9fa4865f58fe492da
Chun Yang <yang2007chun@gmail.com>
2015-11-05 09:20:04 -0600

    add short cut to split spaces

--- zsh/vim.zsh
+++ zsh/vim.zsh
@@ -3,5 +3,5 @@ bindkey -M viins 'jj' vi-cmd-mode
 alias vims='vim -S Session.vim'
 alias v='vim'
 alias vi='vim'
-alias vimsn='cd ~/.vim/bundle/vim-snippets/snippets/'
+alias snippets='cd ~/.vim/bundle/vim-snippets/snippets/'
 export EDITOR='vim'
