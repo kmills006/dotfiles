cbced556d6a57a63fcc16d5fa8dc48330580bb2c
Chun Yang <yang2007chun@gmail.com>
2016-01-27 09:05:18 -0600

    add tmd

--- zsh/tmux.zsh
+++ zsh/tmux.zsh
@@ -5,6 +5,7 @@ alias tms='tmuxinator start'
 alias tmk='tmux kill-session -t'
 alias tmo='tmuxinator open' # create or open a tmuxinator project
 alias tml='tmux list-sessions'
+alias tmd='tmuxinator delete'
 
 fpath=($HOME/.tmuxinator/completion ${fpath})
 autoload -U compinit
