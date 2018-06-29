5e468a8fb53db2cfbae006f6937231abbc21fb9f
Kristy Miller <kristy@kmills.co>
2018-06-29 10:50:35 -0500

    config(alias): add alias for react native android cmds

--- zsh/android.zsh
+++ zsh/android.zsh
@@ -1,2 +1,5 @@
 export ANDROID_HOME="~/Library/Android/sdk"
 export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
+
+alias adt="adb shell input keyevent 82"
+alias adn="adb reverse tcp:8081 tcp:8081"
